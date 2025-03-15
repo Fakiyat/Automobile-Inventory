6b21b6c620b05c6d2918180e235d19981b9d51d3
Fakiyat <fakiyatafaq14@gmail.com>
2025-03-15 23:50:02 +0530

    Frontend

--- Backend/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LogoutView, CustomLoginView , ProfileView
-
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='custom_login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-     path("profile/", ProfileView.as_view(), name="profile")
-]
